92378dbfda21b8b0a2ea7e8d222f54acc4228019
sazanami16 <za_nami.za_nami16@icloud.com>
2021-04-19 18:47:51 +0900

    genre.rbの修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,18 +1,18 @@
 class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--ジャンルを選択--' },
-    { id: 2, name: '小説・評論' },
-    { id: 3, name: 'ビジネス・経済' },
-    { id: 4, name: '歴史・古典' },
-    { id: 5, name: '政治・社会' },
-    { id: 6, name: '建築・デザイン' },
-    { id: 7, name: '暮らし・料理' },
-    { id: 8, name: 'サイエンス・テクノロジー' },
-    { id: 9, name: '教育・自己啓発' },
-    { id: 10, name: '旅行・紀行' },
-    { id: 11, name: '絵本・児童書' },
-    { id: 12, name: 'コミックス' },
-    { id: 13, name: 'ノンフィクション' }
+    { id: 2, name: '〜小説・評論〜' },
+    { id: 3, name: '〜ビジネス・経済〜' },
+    { id: 4, name: '〜歴史・古典〜' },
+    { id: 5, name: '〜政治・社会〜' },
+    { id: 6, name: '〜建築・デザイン〜' },
+    { id: 7, name: '〜暮らし・料理〜' },
+    { id: 8, name: '〜テクノロジー・サイエンス〜' },
+    { id: 9, name: '〜教育・自己啓発〜' },
+    { id: 10, name: '〜旅行・紀行〜' },
+    { id: 11, name: '〜絵本・児童書〜' },
+    { id: 12, name: '〜コミックス〜' },
+    { id: 13, name: '〜ノンフィクション〜' }
   ]
 
     include ActiveHash::Associations
